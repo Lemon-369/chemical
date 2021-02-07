64456080fa625f34efaf879712160673e3861687
Lemon-369 <973780979@qq.com>
2021-02-07 18:41:05 +0800

    2021年2月7日18:40:55

--- src/main/java/com/hy/chemical/ChemicalApplication.java
+++ src/main/java/com/hy/chemical/ChemicalApplication.java
@@ -3,11 +3,15 @@ package com.hy.chemical;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication 标注一个主程序类，表示这是一个Spring Boot 应用
+ */
 @SpringBootApplication
 public class ChemicalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChemicalApplication.class, args);
+        System.out.println("Hello Spring-Boot !");
     }
 
 }
---
+++ src/main/java/com/hy/chemical/pojo/Users.java
@@ -0,0 +1,12 @@
+package com.hy.chemical.pojo;
+
+/**
+ * 管理员/业务员 的实体类
+ */
+
+public class Users {
+    private Integer uid;    //id
+    private String userName;    //账号
+    private String password;    //密码
+    private String type;    //0 管理员，1 业务员
+}
