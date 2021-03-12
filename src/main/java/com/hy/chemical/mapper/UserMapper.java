fcadc6799cac5a38b23d10ee79ea9bfeea306bc9
Lemon-369 <973780979@qq.com>
2021-03-12 10:25:21 +0800

    ~

--- src/main/java/com/hy/chemical/mapper/UserMapper.java
+++ src/main/java/com/hy/chemical/mapper/UserMapper.java
@@ -3,8 +3,8 @@ package com.hy.chemical.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hy.chemical.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
-@Mapper
+@Repository
 public interface UserMapper extends BaseMapper<User> {
-
 }
